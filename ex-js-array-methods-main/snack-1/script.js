338b1ea6df5dedc96997ae4df88e0457b38aee7a
SirioRedGiant <Oiris1992@gmail.com>
2026-01-21 14:18:07 +0100

    Snack 1

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,4 +1,8 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
-
+const names = ["Edoardo", "Simone", "Francesco"];
+names.forEach((name) => console.log(name));
+console.log("========= TRY ALTERNATIVO =========")
+for (const name of names) {
+  console.log(name);
+}
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
