a1ac3140ce60e907374971ebf9bce0e279cf4ad0
SirioRedGiant <Oiris1992@gmail.com>
2026-01-21 17:30:58 +0100

    Snack 7

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -19,5 +19,55 @@ const students = [
   },
 ];
 
+//^ forEach
+let datiStudente;
+students.forEach(function(student) {
+  if (student.id === 2) {
+    datiStudente = student;
+  }
+});
+console.log(datiStudente);
+
+console.log("");
+console.log("===== forEach Arrow function =====");
+
+//note Arrow function forEach
+let dataStudent;
+students.forEach(student => student.id === 2 && (dataStudent = student));
+console.log(dataStudent);
+
+console.log("");
+console.log("==== FIND ====");
+
+//fixed Find
+
+const studentData = students.find(function(student) {
+  return student.id === 2;
+})
+console.log(studentData);
+
+console.log("");
+console.log("==== FIND Arrow function ====");
+
+//* find arrow way
+
+studentDati = students.find(student => student.id === 2);
+console.log(studentDati);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
