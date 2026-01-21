7438e5c3f69d52761cd636dd9b7155f098709a5f
SirioRedGiant <Oiris1992@gmail.com>
2026-01-21 16:02:19 +0100

    Snack 4

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -5,5 +5,51 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+
+//^ forEach
+const arrOfAuthorNames = [];
+posts.forEach(function(post) {
+  arrOfAuthorNames.push(post.author);
+});
+console.log(arrOfAuthorNames)
+
+console.log("");
+console.log("===== ArrowFunction -> forEach =====");
+
+//note forEach Arrow function
+const authorNamesArrow = [];
+
+posts.forEach(post => authorNamesArrow.push(authorNamesArrow.author));
+console.log(authorNamesArrow);
+
+console.log("");
+console.log("===== Map =====");
+
+//fixed Map way
+const namesOfAuthor = posts.map(function(post) {
+  console.log(post.author);
+});
+
+console.log("");
+console.log("===== Arrow Function Map =====");
+
+//* Map in Arrow f
+const authorsNames = posts.map(post => console.log(post.author)
+);
+
+console.log("");
+console.log("===== Mission Completed... =====");
+console.log(arrOfAuthorNames.join(", "));
+
+
+
+
+
+
+
+
+
+
+
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
