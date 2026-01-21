4f1aecfaaadd1cd95a1c7f87b88d632336b91db2
SirioRedGiant <Oiris1992@gmail.com>
2026-01-21 15:04:43 +0100

    Snack 2

--- ex-js-array-methods-main/snack-2/script.js
+++ ex-js-array-methods-main/snack-2/script.js
@@ -1,8 +1,52 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
+
+
+
+//^ Primo
+people.forEach(function(person) {
+  console.log(person.name);
+});
+console.log("");
+
+console.log(`Altro modo di svolgerlo:`);
+//! Secondo
+people.forEach((person) => console.log(person.name));
+console.log("");
+console.log(`Altro modo di svolgerlo:`);
+
+
+//note Terzo
+
+const onlyNames = people.map(function (person) {
+  return person.name;
+});
+console.log(onlyNames.join(", "));
+console.log("");
+console.log(`Altro modo di svolgerlo:`);
+
+
+
+
+//* Quarto
+
+const namesOnly = people.map(person => {
+  return person.name;
+})
+console.log(namesOnly.join(", "));
+console.log("");
+console.log(`Altro modo di svolgerlo:`);
+
+
+
+
+//? Quinto
+
+const nomeDiOgniPersona = people.map(person => person.name);
+console.log(nomeDiOgniPersona.join(", "));
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
